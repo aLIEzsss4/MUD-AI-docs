4d696974dd738d906570514a39eed3791c556b95
aLIEzsss4 <vuetomato@gmail.com>
2023-07-01 08:28:25 +0800

    fix: isloading cannot input

--- src/ui/prompt-form.tsx
+++ src/ui/prompt-form.tsx
@@ -58,7 +58,7 @@ export default function PromptForm({
           onKeyDown={onKeyDown}
           rows={1}
           value={input}
-          onChange={e => setInput(e.target.value)}
+          onChange={e =>isLoading?null:setInput(e.target.value)}
           placeholder="Ask MUD AI"
           spellCheck={false}
           className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
